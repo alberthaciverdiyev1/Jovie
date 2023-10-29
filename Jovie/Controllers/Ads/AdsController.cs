eb5cf6f0eec77c4bbd8c422f54f4a404026ec290
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2023-10-29 20:38:31 +0400

    home page html

---
+++ Jovie/Controllers/Ads/AdsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jovie.Controllers.Ads
+{
+    public class AdsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
